22ec2cdf25c56b282237bba5be705a76c8b739cb
Your Name <you@example.com>
2022-05-16 13:19:25 +0530

    [Megha] Add: Welcome Ton garbage Collection Demo

---
+++ GarbageDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GarbageDemo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            long mem1 = GC.GetTotalMemory(false);
+            {
+                int[] values = new int[5000];
+                values = null;
+            }
+
+            long mem2 = GC.GetTotalMemory(false);
+            {
+                GC.Collect();
+            }
+            long mem3 = GC.GetTotalMemory(false);
+            {
+                Console.WriteLine(mem1);
+                Console.WriteLine(mem2);
+                Console.WriteLine(mem3);
+            }
+            Console.WriteLine("Welcome to GarbageCollection");
+            long bytes1 = GC.GetTotalMemory(false);
+            byte[] memory = new byte[1000 * 1000 * 10];
+            memory[0] = 1;
+            long bytes2 = GC.GetTotalMemory(false);
+            long bytes3 = GC.GetTotalMemory(false);
+            Console.WriteLine(bytes1);
+            Console.WriteLine(bytes2);
+            Console.WriteLine(bytes2 - bytes1);
+            Console.WriteLine(bytes3);
+            Console.WriteLine(bytes3 - bytes2);
+            Console.WriteLine();
+        }
+    }
+}
